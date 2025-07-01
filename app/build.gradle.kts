f70a7c29af7037010a0fc83cb070300fe95921f1
Vladimir Ponomarenko <vova102204@gmail.com>
2025-07-01 22:24:11 +0700

    new version of the application - 2.3.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "com.example.login"
         minSdk = 24
         targetSdk = 34
-        versionCode = 22
-        versionName = "2.2"
+        versionCode = 23
+        versionName = "2.3.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
